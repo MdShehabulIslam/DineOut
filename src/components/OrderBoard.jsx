62ce6afdc77596bba01893ab251d9b8d0948c8c4
Mohammad Shehabul Islam <107636160+MdShehabulIslam@users.noreply.github.com>
2025-05-15 01:45:16 -0230

    added order summary functionalities

--- src/components/OrderBoard.jsx
+++ src/components/OrderBoard.jsx
@@ -63,7 +63,7 @@ export default function OrderBoard() {
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6 flex-grow">
       <CreateOrder />
       <div className="md:col-span-2 h-[calc(100vh_-_130px)]">
-        <OrderSummary />
+        <OrderSummary orders={orders} />
         <OrderReport
           orders={filteredOrders}
           onSearch={handleSearch}
